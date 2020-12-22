b26ce263d584ab23daa3081e7f1398ea99214924
S460 <aleksandar.ta6ev@gmail.com>
2020-12-22 12:49:47 +0200

    I can finnaly create the ******* todos

--- src/todoLogic.js
+++ src/todoLogic.js
@@ -4,6 +4,12 @@ const todoLogic = (function () {
 	const todoPopUp = document.getElementById("todoPopUp");
 	const todoForm = document.getElementById("todoForm");
 
+	const clearTodoDiv = function (div) {
+		while (div.childNodes.length !== 1) {
+			div.removeChild(div.lastChild);
+		}
+	};
+
 	const renderTodos = function (todoList) {
 		todoList.forEach((todo) => {
 			todo.displayTodo();
@@ -11,15 +17,19 @@ const todoLogic = (function () {
 	};
 
 	const createTodo = function (project) {
-		todoForm.addEventListener("submit", (e) => {
+		const handleCreateTodo = function (e) {
 			console.log("createTodoTest");
 			e.preventDefault();
 			let newTodo = todoFactory(project.todoDiv);
 			project.todoList.push(newTodo);
+			clearTodoDiv(project.todoDiv);
 			renderTodos(project.todoList);
 			todoForm.reset();
 			todoPopUp.style.cssText = "display: none";
-		});
+			todoForm.removeEventListener("submit", handleCreateTodo);
+		};
+
+		todoForm.addEventListener("submit", handleCreateTodo);
 	};
 
 	return { createTodo };
